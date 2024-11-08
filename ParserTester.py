92a83e54fa07a988a82caa0ee1abff2bbfd6a230
weston1111 <wes32315@gmail.com>
2024-11-08 16:51:08 -0600

    initial file from before branch deletion lol

--- ParserTester.py
+++ ParserTester.py
@@ -1,4 +1,19 @@
 import sys
 from antlr4 import *
-from PythonLexer import PythonLexer
-from PythonParser import PythonParser
+from Deliverable1Parser import Deliverable1Parser
+from Deliverable1Lexer import Deliverable1Lexer
+from antlr4.tree.Trees import Trees
+
+file_path = "./project_deliverable_1.py"
+
+def main():
+    input_stream = FileStream(file_path)
+    lexer = Deliverable1Lexer(input_stream)
+    token_stream = CommonTokenStream(lexer)
+    parser = Deliverable1Parser(token_stream)
+    tree = parser.prog()
+    print(tree.toStringTree(recog=parser))
+
+main()
+    
+
